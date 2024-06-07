25d41b6921305510f2e8eb79ef22604958a9af1c
DevKumar Chauhan <104253026+DEVC0212@users.noreply.github.com>
2024-06-07 16:10:05 +0530

    final one

---
+++ index.js
@@ -0,0 +1,104 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./src/utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./src/utils/users')
+const { getMessages, createRoom,sendMessage } = require('./src/utils/UserModel')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '/')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if (error) {
+            return callback(error)
+        }
+
+// createRoom({
+//     roomName: user.room,
+//     roomCode: '1234'
+// }).then((response) => {
+//     console.log(response);
+//     isExist = true;
+
+//     // Now that the room has been created, get the messages
+//     return getMessages(user.room); // Assuming getMessages returns a promise
+// }).then((messages) => {
+//     console.log(messages);
+//     if (messages && messages.length >  0) {
+//         messages.forEach((message) => {
+//             socket.emit('message', generateMessage(message.username, message.message));
+//         });
+//     }
+// }).catch((error) => {
+//     console.error('Error:', error);
+// });
+
+socket.join(user.room);
+socket.emit('message', generateMessage('Admin', 'Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        
+        })
+
+        callback()
+
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        // sendMessage({
+        //     roomName: user.room,
+        //     message: {
+        //         username: user.username,
+        //         message: message,
+        //         createdAt: new Date()
+        //     }
+        // })
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
---
+++ src/utils/UserModel.js
@@ -0,0 +1,147 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/chat-app');
+
+// const User = mongoose.model('User',{
+//     name: {
+//         type: String
+//     },
+//     roomName:{
+//         type: String
+//     }
+// })
+
+// const Room = mongoose.model('Room',{
+//     roomName: {
+//         type: String
+//     },
+//     roomCode: {
+//         type: String
+//     },
+//     messages: [{
+//         username: {
+//             type: String
+//         },
+//         message: {
+//             type: String
+//         },
+//         createdAt: {
+//             type: Date
+//         }
+//     }]
+// })
+
+// async function createRoom(params) {
+//     //console.log(params);
+// //  If room exists go to room or else create one
+// try {
+//     const room = await Room.findOne({ roomName: params.roomName });
+//     if (room) {
+//         //console.log('Room already exists');
+//         // Assuming you want to return the room's message or some data
+//         // For demonstration, let's return the room object
+//         return room;
+//     } else {
+//         //console.log('Room does not exist');
+        
+//     const room = new Room(params);
+//     room.save().then((response)=>{
+//         //console.log(response);
+//     }).catch((error)=>{
+//         //console.log(error);
+//     })
+//     return false
+//         return false;
+//     }
+// } catch (error) {
+//     console.error('Error checking room:', error);
+//     // Handle the error appropriately
+//     return false;
+// }
+// }
+
+// function sendMessage(params) {
+//     // Get message from params and store in Room
+//     Room.findOne({roomName: params.roomName}).then((room)=>{
+//         room.messages.push(params.message);
+//         room.save().then((response)=>{
+//             //console.log(response);
+//         }).catch((error)=>{
+//             //console.log(error);
+//         })
+//     }).catch((error)=>{
+//         //console.log(error);
+//     })
+// }
+
+// async function getMessages(roomName) {
+//     // Room.findOne({roomName}).then((room)=>{
+//     //     // //console.log("IN APIS :::",room);
+//     //     return room.messages;
+//     // }).catch((error)=>{
+//     //     //console.log(error);
+//     console.log("IN GETMSG");
+//     // }
+//     try {
+
+//             const room = await Room.findOne({roomName});
+//             console.log(room);
+//             return room ? room.messages : null;
+//         } catch (error) {
+//             console.log(error);
+//             return null; // or handle the error as needed
+//         }
+//     }
+    
+
+// // createRoom({
+// //     roomName: 'Room1',
+// //     roomCode: '1234'
+// // })
+
+// // createRoom({
+// //     roomName: 'Room2',
+// //     roomCode: '5678'
+// // })
+
+// // sendMessage({
+// //     roomName: 'Room1',
+// //     message: {
+// //         username: 'Dev',
+// //         message: 'Hello',
+// //         createdAt: new Date()
+// //     }
+// // })
+// // sendMessage({
+// //     roomName: 'Room1',
+// //     message: {
+// //         username: 'Smit',
+// //         message: 'Hello Dev!',
+// //         createdAt: new Date()
+// //     }
+// // })
+
+// // sendMessage({
+// //     roomName: 'Room2',
+// //     message: {
+// //         username: 'DK',
+// //         message: 'Fine!',
+// //         createdAt: new Date()
+// //     }
+// // })
+// const me = new User({
+//     name: 'Dev',
+//     roomName: 'Room3'
+// })
+
+// // me.save().then((response)=>{
+// //     //console.log(response);
+// // }).catch((error)=>{
+// //     //console.log(error);
+// // })
+
+// module.exports = {
+//     createRoom,
+//     sendMessage,
+//     getMessages
+// }
